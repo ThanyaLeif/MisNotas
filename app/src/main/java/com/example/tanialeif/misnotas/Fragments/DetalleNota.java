a9b753a2c0551f0dd8d1fb9e969fe9cf6d8ee893
ThanyaLeif <32908302+ThanyaLeif@users.noreply.github.com>
2018-10-27 18:04:02 -0500

    Agregando funcionalidad a la ventana de detalles de la nota

---
+++ app/src/main/java/com/example/tanialeif/misnotas/Fragments/DetalleNota.java
@@ -0,0 +1,60 @@
+package com.example.tanialeif.misnotas.Fragments;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.tanialeif.misnotas.R;
+
+import java.util.Calendar;
+
+public class DetalleNota extends Fragment implements View.OnClickListener {
+
+    EditText txtFecha, txtHora;
+    Button btnFecha, btnHora;
+
+    private int dia, mes, anio, hora, minutos;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detalle_nota, container, false);
+
+        btnFecha = (Button)view.findViewById(R.id.btnFecha);
+        btnHora = (Button)view.findViewById(R.id.btnHora);
+        txtFecha = (EditText)view.findViewById(R.id.txtFecha);
+        txtHora = (EditText)view.findViewById(R.id.txtHora);
+
+        btnFecha.setOnClickListener(this);
+        btnHora.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btnFecha){
+            final Calendar calendar =Calendar.getInstance();
+            dia = calendar.get(Calendar.DAY_OF_MONTH);
+            mes = calendar.get(Calendar.MONTH);
+            anio = calendar.get(Calendar.YEAR);
+
+            
+        }
+        if(v==btnHora){
+
+        }
+    }
+}
