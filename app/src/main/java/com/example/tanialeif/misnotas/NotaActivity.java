8be15be41cfdb3800d438bebe1c7ee64e1b40e51
ThanyaLeif <32908302+ThanyaLeif@users.noreply.github.com>
2018-11-04 20:42:46 -0600

    Se agregaron permisos de audio y cámara.

--- app/src/main/java/com/example/tanialeif/misnotas/NotaActivity.java
+++ app/src/main/java/com/example/tanialeif/misnotas/NotaActivity.java
@@ -1,16 +1,26 @@
 package com.example.tanialeif.misnotas;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.tanialeif.misnotas.Adapters.ListMemoAdapter;
 import com.example.tanialeif.misnotas.DB.DAOMemo;
@@ -20,17 +30,23 @@ import com.example.tanialeif.misnotas.Model.Note;
 
 import java.util.ArrayList;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.CAMERA;
+
 public class NotaActivity extends AppCompatActivity {
 
     RecyclerView list;
     ListMemoAdapter adapter;
 
-    Button btnGuardar;
+    Button btnGuardar, btnAgregarMultimedia;
     EditText txtTitulo, txtDescripcion, txtFecha, txtHora;
 
     int REQ_NEW_NOTE = 1;
     int REQ_MOD_NOTE = 2;
     int ID_NOTA = 1;
+
+    boolean allowetToUseCamera = false;
+    boolean allowetToUseAudio = false;
 
     private ArrayList<Memo> temporalStaticListExample() {
         ArrayList<Memo> listMemo = new ArrayList<>();
@@ -76,6 +92,49 @@ public class NotaActivity extends AppCompatActivity {
                 }
             }
         );
+
+        btnAgregarMultimedia = (Button) findViewById(R.id.btnAgregarMultimedia);
+
+        btnAgregarMultimedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence[] options = new CharSequence[]  {
+                        "Audio",
+                        "Camara"
+                };
+                AlertDialog.Builder menu = new AlertDialog.Builder(self);
+
+                menu.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0: {
+                                validarAudio();
+                                if(allowetToUseAudio) {
+                                    Intent detail = new Intent(self, AudioRecorderActivity.class);
+                                    startActivity(detail);
+                                }
+                                else{
+                                    Toast.makeText(self,"No hay permisos suficientes",Toast.LENGTH_SHORT).show();
+                                }
+                                break;
+                            }
+                            case 1: {
+                                validarCamara();;
+                                if(allowetToUseCamera) {
+                                    Intent detail = new Intent(self, PhotoActivity.class);
+                                    startActivity(detail);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                });
+
+                menu.show();
+            }
+
+        });
 
         daoMemo.insert(temporalStaticListExample().get(0));
         daoMemo.insert(temporalStaticListExample().get(1));
@@ -126,6 +185,95 @@ public class NotaActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    private void validarCamara(){
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            allowetToUseCamera = true;
+        }
+
+        else if(ContextCompat.checkSelfPermission(NotaActivity.this,
+                Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
+            allowetToUseCamera = true;
+        }
+
+        else if((ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA))){
+            new AlertDialog.Builder(this)
+                    .setTitle("Se necesitan permisos")
+                    .setMessage("Habilite los permisos para el correcto funcionamiento de la app")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(NotaActivity.this, new String[]{Manifest.permission.CAMERA},100);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+            .create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},100);
+        }
+    }
+
+    private void validarAudio(){
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            allowetToUseAudio = true;
+        }
+
+        else if(ContextCompat.checkSelfPermission(NotaActivity.this,
+                Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_GRANTED){
+            allowetToUseAudio = true;
+        }
+
+        else if((ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.RECORD_AUDIO))){
+            new AlertDialog.Builder(this)
+                    .setTitle("Se necesitan permisos")
+                    .setMessage("Habilite los permisos para el correcto funcionamiento de la app")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(NotaActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},101);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                allowetToUseCamera = true;
+            }
+            else{
+                allowetToUseCamera=false;
+            }
+        }
+        if(requestCode==101){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                allowetToUseAudio = true;
+            }
+            else{
+                allowetToUseAudio=false;
+            }
+        }
     }
 
     public long insertNote(){
